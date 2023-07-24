d357857e763c93a5e6a6571419c956c0003ee048
Kunal Borkar <137871453+kunalborkar2001@users.noreply.github.com>
2023-07-24 12:44:47 +0530

    Star Bucks Landing Page

---
+++ index.js
@@ -0,0 +1,8 @@
+let imageSlider = (anything) => {
+    document.querySelector(".starbucks").src = anything
+}
+
+let changeColor = (color) => {
+    let circle = document.querySelector(".circle")
+    circle.style.background = color;
+}
